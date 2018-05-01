6d0ac9c6a02d9dd951317498aa2cd55ab89ea144
manitocode <3047966816@qq.com>
2018-05-01 10:16:18 +0800

    阿哈算法

---
+++ c_test/maopao.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	/*冒泡排序1*/
+	int a[100], i, j, t, n;
+	scanf("%d",&n);
+	for(i = 1;i <= n; i++)
+	{
+		scanf("%d",&a[i]);
+	}
+
+	for(i=1;i <= n-1;i++)
+	{
+		for(j=1;j<=n-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				t=a[j];
+				a[j]=a[j+1];
+				a[j+1]=t;
+			}
+		}
+	}
+
+	for(i=1;i<=n;i++)
+	{
+		printf("%d ", a[i]);
+	}
+	getchar();getchar();
+	return 0;
+}
