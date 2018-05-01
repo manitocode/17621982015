07d935c22ceac19269c7e6f4afa9bf94f8b9e856
manitocode <3047966816@qq.com>
2018-05-01 10:20:49 +0800

    阿哈算法

--- c_test/maopao.c
+++ c_test/maopao.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
+struct student {
+	char name[21];
+	int score;
+};
+
 int main()
 {
 	/*冒泡排序1*/
-	int a[100], i, j, t, n;
+	struct student a[100],t;
+	int i, j, n;
 	scanf("%d",&n);
 	for(i = 1;i <= n; i++)
 	{
-		scanf("%d",&a[i]);
+		scanf("%s %d",a[i].name, &a[i].score);
 	}
 
 	for(i=1;i <= n-1;i++)
 	{
 		for(j=1;j<=n-1;j++)
 		{
-			if(a[j]>a[j+1])
+			if(a[j].score>a[j+1].score)
 			{
 				t=a[j];
 				a[j]=a[j+1];
@@ -25,7 +31,7 @@ int main()
 
 	for(i=1;i<=n;i++)
 	{
-		printf("%d ", a[i]);
+		printf("%s %d\n", a[i].name, a[i].score);
 	}
 	getchar();getchar();
 	return 0;
